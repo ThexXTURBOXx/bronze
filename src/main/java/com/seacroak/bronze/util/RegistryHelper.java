af72cd2e469674c5efae044e978076c26a7485c2
Khazoda <khazoda@gmail.com>
2024-05-05 11:42:40 +0100

    registration helper class

---
+++ src/main/java/com/seacroak/bronze/util/RegistryHelper.java
@@ -0,0 +1,61 @@
+package com.seacroak.bronze.util;
+
+import com.seacroak.bronze.BronzeMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import static com.seacroak.bronze.Constants.BRONZE_ID;
+
+public class RegistryHelper {
+
+  // General use Identifier() maker function
+  public static Identifier newID(String name) {
+    return new Identifier(BRONZE_ID, name);
+  }
+
+  // Block Registry Helper Functions
+  // *******************************
+  // 1. Default BlockItem Registration Entrypoint: creates Identifier from ModID & block name
+  public static <B extends Block> B registerBlock(String name, B block, Item.Settings itemSettings) {
+    return registerBlock(newID(name), block, itemSettings);
+  }
+
+  // 2. Takes identifier and registers block and block items
+  public static <B extends Block> B registerBlock(Identifier name, B block, Item.Settings itemSettings) {
+    BlockItem item = new BlockItem(block, (itemSettings));
+    item.appendBlocks(Item.BLOCK_ITEMS, item);
+
+    Registry.register(Registries.BLOCK, name, block);
+    Registry.register(Registries.ITEM, name, item);
+    return block;
+  }
+
+  public static <B extends Block> B registerBlockOnly(String name, B block) {
+    return registerBlockOnly(newID(name), block);
+  }
+
+  public static <B extends Block> B registerBlockOnly(Identifier name, B block) {
+    Registry.register(Registries.BLOCK, name, block);
+    return block;
+  }
+
+  public static <I extends BlockItem> I registerBlockItem(String name, I blockItem) {
+    return registerBlockItem(newID(name), blockItem);
+  }
+
+  public static <I extends BlockItem> I registerBlockItem(Identifier name, I blockItem) {
+    Registry.register(Registries.ITEM, name, blockItem);
+    return blockItem;
+  }
+
+  // Item Registry Helper Functions
+  // ******************************
+  public static Item registerItem(String name, Item item) {
+    return Registry.register(Registries.ITEM, newID(name), item);
+
+  }
+}
