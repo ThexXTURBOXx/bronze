58139c203b506dbb22bb4143a61038c86df0b7ad
Khazoda <khazoda@gmail.com>
2024-05-05 23:59:27 +0100

    + bronze blend base

---
+++ src/main/java/com/seacroak/bronze/item/BronzeBlend.java
@@ -0,0 +1,9 @@
+package com.seacroak.bronze.item;
+
+import net.minecraft.item.Item;
+
+public class BronzeBlend extends Item {
+  public BronzeBlend(Settings settings) {
+    super(settings);
+  }
+}
