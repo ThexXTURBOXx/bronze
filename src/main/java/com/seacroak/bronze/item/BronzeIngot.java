ef866234a179074a399f7657fcac62d575c8e2a9
Khazoda <khazoda@gmail.com>
2024-05-05 11:42:50 +0100

    + bronze ingot base

---
+++ src/main/java/com/seacroak/bronze/item/BronzeIngot.java
@@ -0,0 +1,9 @@
+package com.seacroak.bronze.item;
+
+import net.minecraft.item.Item;
+
+public class BronzeIngot extends Item {
+  public BronzeIngot(Settings settings) {
+    super(settings);
+  }
+}
