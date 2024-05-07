a60c3d07c0eeb9af42e2635e32a8bad9b3f2a90a
Khazoda <khazoda@gmail.com>
2024-05-07 23:53:03 +0100

    misc.

--- src/main/java/com/seacroak/bronze/registry/ItemGroupRegistry.java
+++ src/main/java/com/seacroak/bronze/registry/ItemGroupRegistry.java
@@ -15,23 +15,6 @@ public class ItemGroupRegistry {
         .icon(() -> new ItemStack(MainRegistry.BRONZE_INGOT))
         .displayName(Text.translatable("bronze.itemGroup"))
         .entries((displayContext, entries) -> {
-          entries.add(new ItemStack(MainRegistry.TIN_BLOCK));
-          entries.add(new ItemStack(MainRegistry.TIN_ORE));
-          entries.add(new ItemStack(MainRegistry.DEEPSLATE_TIN_ORE));
-          entries.add(new ItemStack(MainRegistry.TIN_INGOT));
-          entries.add(new ItemStack(MainRegistry.TIN_RAW_BLOCK));
-          entries.add(new ItemStack(MainRegistry.TIN_RAW));
-
-          entries.add(new ItemStack(MainRegistry.BRONZE_BLOCK));
-          entries.add(new ItemStack(MainRegistry.BRONZE_INGOT));
-          entries.add(new ItemStack(MainRegistry.BRONZE_NUGGET));
-          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND_BLOCK));
-          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND));
-
-          entries.add(new ItemStack(MainRegistry.BRONZE_HELMET));
-          entries.add(new ItemStack(MainRegistry.BRONZE_CHESTPLATE));
-          entries.add(new ItemStack(MainRegistry.BRONZE_LEGGINGS));
-          entries.add(new ItemStack(MainRegistry.BRONZE_BOOTS));
 
           entries.add(new ItemStack(MainRegistry.BRONZE_SWORD));
           entries.add(new ItemStack(MainRegistry.BRONZE_AXE));
@@ -39,9 +22,24 @@ public class ItemGroupRegistry {
           entries.add(new ItemStack(MainRegistry.BRONZE_SHOVEL));
           entries.add(new ItemStack(MainRegistry.BRONZE_HOE));
 
+          entries.add(new ItemStack(MainRegistry.BRONZE_HELMET));
+          entries.add(new ItemStack(MainRegistry.BRONZE_CHESTPLATE));
+          entries.add(new ItemStack(MainRegistry.BRONZE_LEGGINGS));
+          entries.add(new ItemStack(MainRegistry.BRONZE_BOOTS));
+
+          entries.add(new ItemStack(MainRegistry.TIN_BLOCK));
+          entries.add(new ItemStack(MainRegistry.BRONZE_BLOCK));
+          entries.add(new ItemStack(MainRegistry.TIN_RAW_BLOCK));
+          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND_BLOCK));
+          entries.add(new ItemStack(MainRegistry.TIN_ORE));
+          entries.add(new ItemStack(MainRegistry.DEEPSLATE_TIN_ORE));
+          entries.add(new ItemStack(MainRegistry.TIN_RAW));
+          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND));
+          entries.add(new ItemStack(MainRegistry.TIN_INGOT));
+          entries.add(new ItemStack(MainRegistry.BRONZE_INGOT));
+          entries.add(new ItemStack(MainRegistry.BRONZE_NUGGET));
+
           entries.add(new ItemStack(MainRegistry.BRONZE_HORSE_ARMOR));
-
-
         }).build();
   }
 
