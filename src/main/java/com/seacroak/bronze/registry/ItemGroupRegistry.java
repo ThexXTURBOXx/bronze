c51e83f73b5424de2cda05cd3b6c60ffb4cafd97
Khazoda <khazoda@gmail.com>
2024-05-07 17:21:58 +0100

    vanilla itemGroup integration

--- src/main/java/com/seacroak/bronze/registry/ItemGroupRegistry.java
+++ src/main/java/com/seacroak/bronze/registry/ItemGroupRegistry.java
@@ -1,8 +1,11 @@
 package com.seacroak.bronze.registry;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 
 
@@ -13,16 +16,17 @@ public class ItemGroupRegistry {
         .displayName(Text.translatable("bronze.itemGroup"))
         .entries((displayContext, entries) -> {
           entries.add(new ItemStack(MainRegistry.TIN_BLOCK));
-          entries.add(new ItemStack(MainRegistry.BRONZE_BLOCK));
           entries.add(new ItemStack(MainRegistry.TIN_ORE));
           entries.add(new ItemStack(MainRegistry.DEEPSLATE_TIN_ORE));
-          entries.add(new ItemStack(MainRegistry.TIN_RAW_BLOCK));
-          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND_BLOCK));
-          entries.add(new ItemStack(MainRegistry.TIN_RAW));
-          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND));
-          entries.add(new ItemStack(MainRegistry.BRONZE_NUGGET));
           entries.add(new ItemStack(MainRegistry.TIN_INGOT));
+          entries.add(new ItemStack(MainRegistry.TIN_RAW_BLOCK));
+          entries.add(new ItemStack(MainRegistry.TIN_RAW));
+
+          entries.add(new ItemStack(MainRegistry.BRONZE_BLOCK));
           entries.add(new ItemStack(MainRegistry.BRONZE_INGOT));
+          entries.add(new ItemStack(MainRegistry.BRONZE_NUGGET));
+          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND_BLOCK));
+          entries.add(new ItemStack(MainRegistry.BRONZE_BLEND));
 
           entries.add(new ItemStack(MainRegistry.BRONZE_HELMET));
           entries.add(new ItemStack(MainRegistry.BRONZE_CHESTPLATE));
@@ -41,4 +45,68 @@ public class ItemGroupRegistry {
         }).build();
   }
 
+  /* Method needs to be manually updated with the vanilla itemGroups each entry should reside in */
+  public static void slideItemsIntoVanillaItemGroups() {
+
+    /* Tools */
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+      content.addAfter(Items.IRON_HOE,
+          MainRegistry.BRONZE_SHOVEL,
+          MainRegistry.BRONZE_PICKAXE,
+          MainRegistry.BRONZE_AXE,
+          MainRegistry.BRONZE_HOE);
+    });
+
+    /* Combat */
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+      content.addAfter(Items.IRON_SWORD,
+          MainRegistry.BRONZE_SWORD);
+
+      content.addAfter(Items.IRON_AXE,
+          MainRegistry.BRONZE_AXE);
+
+      content.addAfter(Items.IRON_BOOTS,
+          MainRegistry.BRONZE_HELMET,
+          MainRegistry.BRONZE_CHESTPLATE,
+          MainRegistry.BRONZE_LEGGINGS,
+          MainRegistry.BRONZE_BOOTS);
+
+      content.addAfter(Items.IRON_HORSE_ARMOR,
+          MainRegistry.BRONZE_HORSE_ARMOR);
+    });
+
+    /* Ingredients */
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+      content.addAfter(Items.RAW_IRON,
+          MainRegistry.BRONZE_BLEND,
+          MainRegistry.TIN_RAW);
+
+      content.addAfter(Items.IRON_NUGGET,
+          MainRegistry.BRONZE_NUGGET);
+
+      content.addAfter(Items.IRON_INGOT,
+          MainRegistry.BRONZE_INGOT,
+          MainRegistry.TIN_INGOT);
+    });
+
+    /* Natural Blocks */
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(content -> {
+      content.addAfter(Items.DEEPSLATE_IRON_ORE,
+          MainRegistry.TIN_ORE,
+          MainRegistry.DEEPSLATE_TIN_ORE);
+
+      content.addAfter(Items.RAW_IRON_BLOCK,
+          MainRegistry.BRONZE_BLEND_BLOCK,
+          MainRegistry.TIN_RAW_BLOCK);
+    });
+
+    /* Building Blocks */
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+      content.addAfter(Items.CHAIN,
+          MainRegistry.BRONZE_BLOCK,
+          MainRegistry.TIN_BLOCK);
+    });
+
+
+  }
 }
