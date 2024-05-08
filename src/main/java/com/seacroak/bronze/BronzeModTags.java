f5a24703c7b11ea4250126c7fcd5305060d4947a
Khazoda <khazoda@gmail.com>
2024-05-08 20:37:59 +0100

    adding some common tags for intercompatibility

---
+++ src/main/java/com/seacroak/bronze/BronzeModTags.java
@@ -0,0 +1,21 @@
+package com.seacroak.bronze;
+
+import com.seacroak.bronze.util.GenericUtils;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+
+import static com.seacroak.bronze.util.GenericUtils.ID;
+
+public class BronzeModTags {
+
+  public static class Block {
+    public static final TagKey<net.minecraft.block.Block> TIN_ORES = TagKey.of(RegistryKeys.BLOCK, ID( "tin_ores"));
+
+  }
+
+  public static class Items {
+
+  }
+
+}
