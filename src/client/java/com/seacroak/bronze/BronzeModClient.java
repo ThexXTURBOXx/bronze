8abed473f9236ef1029e070b1531ddddd2eeaf17
Khazoda <khazoda@gmail.com>
2024-05-09 19:08:30 +0100

    + bronze trapdoor render mode

--- src/client/java/com/seacroak/bronze/BronzeModClient.java
+++ src/client/java/com/seacroak/bronze/BronzeModClient.java
@@ -1,10 +1,15 @@
 package com.seacroak.bronze;
 
+import com.seacroak.bronze.registry.MainRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class BronzeModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BRONZE_TRAPDOOR, RenderLayer.getCutoutMipped());
+
 	}
 }
