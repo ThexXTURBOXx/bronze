d7b17954df7b2bcb8a8dd0d57e35138933c80b4b
Khazoda <khazoda@gmail.com>
2024-06-01 15:58:04 +0100

    1.21 port compat tweaks

--- src/main/java/com/seacroak/bronze/block/TinBlock.java
+++ src/main/java/com/seacroak/bronze/block/TinBlock.java
@@ -1,11 +1,10 @@
 package com.seacroak.bronze.block;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.enums.Instrument;
+import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.sound.BlockSoundGroup;
 
 public class TinBlock extends Block {
   public TinBlock() {
-    super(AbstractBlock.Settings.create().strength(2.5f).instrument(Instrument.IRON_XYLOPHONE).sounds(BlockSoundGroup.COPPER).requiresTool());  }
+    super(Settings.create().strength(2.5f).instrument(NoteBlockInstrument.IRON_XYLOPHONE).sounds(BlockSoundGroup.COPPER).requiresTool());  }
 }
